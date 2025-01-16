b59b20ed95068b604b3caf32c4fc01244f8ede00
Gbemiro Jiboye <gbenroscience@yahoo.com>
2025-01-16 12:04:07 +0100

    fixed bug in ForEach

--- tests/list_foreach_test.go
+++ tests/list_foreach_test.go
@@ -2,6 +2,8 @@ package tests
 
 import (
 	"fmt"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 
@@ -14,19 +16,36 @@ func TestForEach(t *testing.T) {
 
 	add(list, 1000)
 
-	go list.ForEach(func(val int) bool {
-		fmt.Printf("val-1: %+v\n", val)
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	list.ForEach(func(val int) bool {
+		fmt.Printf("val-0: %+v\n", val)
 		return true
 	})
 
-	go list.ForEach(func(val int) bool {
-		fmt.Printf("val-2: %+v\n", val)
-		return true
-	})
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		list.ForEach(func(val int) bool {
+			fmt.Printf("val-1: %+v\n", val)
+			return true
+		})
+		fmt.Println("First goroutine done")
+	}(&wg)
+	time.Sleep(5 * time.Second)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		list.ForEach(func(val int) bool {
+			fmt.Printf("val-2: %+v\n", val)
+			return true
+		})
+		fmt.Println("Second goroutine done")
+	}(&wg)
 
-	time.Sleep(time.Second * 10)
+	wg.Wait()
 
-	fmt.Println("Tests done")
+	fmt.Println("All tests done")
 
 }
 
@@ -34,4 +53,5 @@ func add(list *ds.AnyList[int], itemCount int) {
 	for i := 0; i < itemCount; i++ {
 		list.Add(i)
 	}
+	fmt.Println("Done adding " + strconv.Itoa(itemCount) + " items to list")
 }
